303ecbf15baade8fa7bcb7ce19dfffab03cd1936
Shinichiro Kuwahara <kenbunroku524@gmail.com>
2022-02-18 08:54:08 -0600

    Add Banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css'
 import { Row } from './conponents/Row'
+import { Banner } from './conponents/Banner'
 import { requests } from './request'
 
 function App() {
   return (
     <div className="App">
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchUrl={requests.feachNetflixOriginals}
